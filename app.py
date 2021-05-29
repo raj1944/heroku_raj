5aadbabd2e915107bed4ee4d0d4318be3d466276
raj1944 <rajakbari441999@gmail.com>
2021-05-29 14:31:12 +0530

    1

--- app.py
+++ app.py
@@ -1,33 +1,38 @@
+
+from sqlalchemy import *
+from config import host, port, database, user, password
+import psycopg2
 from flask import Flask, request
-# import psycopg2
-
 app = Flask(__name__)
+conn_str = f"postgresql://{user}:{password}@{host}/{database}"
+engine = create_engine(conn_str)
+connection = engine.connect()
+# conn = psycopg2.connect(database="test", user = "princepansheriya", password = "prince", host = "127.0.0.1", port = "5432")
 
-# conn = psycopg2.connect(database="test2", user = "postgres", password = "raj", host = "127.0.0.1", port = "5432")
-
-# print("Opened database successfully")
-
-# cur = conn.cursor()
-
+print ("Opened database successfully")
+# cur = connection.cursor()
 @app.route('/api/branches/autocomplete')
 def hello():
-#     args = request.args
-#     print(args['q'])
-#     query = 'SELECT * from branches' 
-#     print(query)  
-#     cur.execute(query)
-#     rows = cur.fetchall()
-#     json_data = {}
-#     json_data['banks'] = []
-#     for x in rows:
-#         print(x)
-#         json_data['banks'].append(list(x))
-#     print(json_data)
-#     return json_data
+    # return 'prince'
+    args = request.args
+    print(args['q'])
+    # query = 'SELECT * from branches where branch="RTGS-HO"'
+    s = ""
+    s += "SELECT * from branches "
+    s += "WHERE"
+    s += "("
+    s += " branch = '" + args['q'] + "'"
+    s += ")"
+    print(s)  
+    rows = connection.execute(s)
+    # rows = connection.fetchall()
+    json_data = {}
+    json_data['banks'] = []
+    for x in rows:
+        # print(x)
+        json_data['banks'].append(list(x))
+    print(json_data)
+    return json_data
 
-# @app.route('/')
-# def home_test():
-#     return "<h1>hello</h1>"
 
-# if __name__ == '__main__':
-#     app.run()
+
---
+++ config.py
@@ -0,0 +1,5 @@
+host = "ec2-52-0-114-209.compute-1.amazonaws.com"
+port = 5432
+database = "d49psqbi5tms6g"
+user = "mihbfwdrnefkbg"
+password = "c8945e997536075b3fdb1f9f9c87471521b4e6e1be9bfac194f871547b320e92"
